0fed48fbf00369b91fbefa49df98c31a278624ad
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 <48620486+CondeSaheki@users.noreply.github.com>
2024-02-19 01:01:25 -0300

    fix lazeroverlay Lerping

--- MeterWay/Overlays/LazerOverlay.cs
+++ MeterWay/Overlays/LazerOverlay.cs
@@ -19,15 +19,6 @@ public class LazerOverlay : IMeterwayOverlay
     private Vector2 WindowMin { get; set; }
     private Vector2 WindowMax { get; set; }
 
-    public LazerOverlay()
-    {
-        this.WindowMin = new Vector2();
-        this.WindowMax = new Vector2();
-
-        this.combat = new Encounter();
-        this.sortCache = new List<uint>();
-    }
-
     class LerpPlayerData
     {
         public double DPS { get; set; }
@@ -36,88 +27,35 @@ public class LazerOverlay : IMeterwayOverlay
         public double Position { get; set; }
     }
 
+    private double transitionDuration = 0.3f; // in seconds
+    private double transitionTimer = 0.0f;
+    private Dictionary<uint, LerpPlayerData> lerpedInfo;
+    private Dictionary<uint, LerpPlayerData> targetInfo;
 
+    public LazerOverlay()
+    {
+        this.WindowMin = new Vector2();
+        this.WindowMax = new Vector2();
+
+        this.lerpedInfo = new Dictionary<uint, LerpPlayerData>();
+        this.targetInfo = new Dictionary<uint, LerpPlayerData>();
+
+        this.combat = new Encounter();
+        this.sortCache = new List<uint>();
+    }
+    
     public void DataProcess(Encounter data)
     {
-        if (data.id != this.combat.id) this.sortCache = Helpers.CreateDictionarySortCache(data.Players);
+        if (data.id != this.combat.id)
+        {
+            lerpedInfo = new Dictionary<uint, LerpPlayerData>();
+            targetInfo = new Dictionary<uint, LerpPlayerData>();
+            this.sortCache = Helpers.CreateDictionarySortCache(data.Players);
+        }
         this.combat.Update();
 
         sortCache.Sort((uint first, uint second) => { return data.Players[second].TotalDamage.CompareTo(data.Players[first].TotalDamage); });
         this.combat = data;
-    }
-
-    private double transitionDuration = 0.3f; // in seconds
-    private double transitionTimer = 0.0f;
-    private Dictionary<string, LerpPlayerData> lerpedInfo = new Dictionary<string, LerpPlayerData>();
-    private Dictionary<string, LerpPlayerData> targetInfo = new Dictionary<string, LerpPlayerData>();
-
-    private void UpdateWindowSize()
-    {
-        Vector2 vMin = ImGui.GetWindowContentRegionMin();
-        Vector2 vMax = ImGui.GetWindowContentRegionMax();
-
-        vMin.X += ImGui.GetWindowPos().X;
-        vMin.Y += ImGui.GetWindowPos().Y;
-        vMax.X += ImGui.GetWindowPos().X;
-        vMax.Y += ImGui.GetWindowPos().Y;
-        this.WindowMin = vMin;
-        this.WindowMax = vMax;
-    }
-
-
-    private void GenerateCombatLerping(Player player)
-    {
-        double topPlayerDamagePercentage = this.combat.Players[sortCache.First()].DamagePercentage;
-        if (topPlayerDamagePercentage == 0)
-        {
-            topPlayerDamagePercentage = 1; // Failsafe to avoid division by zero
-        }
-
-        lerpedInfo[player.Name] = new LerpPlayerData
-        {
-            DPS = player.DPS,
-            PctBar = player.DamagePercentage / topPlayerDamagePercentage * 100,
-            TotalDMG = player.TotalDamage,
-            Position = sortCache.IndexOf(player.Id) + 1
-        };
-
-        targetInfo[player.Name] = new LerpPlayerData
-        {
-            DPS = player.DPS,
-            PctBar = player.DamagePercentage / topPlayerDamagePercentage * 100,
-            TotalDMG = player.TotalDamage,
-            Position = sortCache.IndexOf(player.Id) + 1
-        };
-    }
-
-    private void DoLerpPlayerData(Player player)
-    {
-        if (!lerpedInfo.ContainsKey(player.Name))
-        {
-            GenerateCombatLerping(player);
-        }
-
-        if (transitionTimer < transitionDuration)
-        {
-            transitionTimer += ImGui.GetIO().DeltaTime;
-            double t = transitionTimer / transitionDuration;
-            lerpedInfo[player.Name].DPS = Helpers.Lerp(lerpedInfo[player.Name].DPS, targetInfo[player.Name].DPS, t);
-            lerpedInfo[player.Name].PctBar = Helpers.Lerp(lerpedInfo[player.Name].PctBar, targetInfo[player.Name].PctBar, t);
-            lerpedInfo[player.Name].TotalDMG = Helpers.Lerp(lerpedInfo[player.Name].TotalDMG, targetInfo[player.Name].TotalDMG, t);
-            lerpedInfo[player.Name].Position = Helpers.Lerp(lerpedInfo[player.Name].Position, targetInfo[player.Name].Position, t);
-        }
-
-        if (targetInfo[player.Name].TotalDMG <= player.TotalDamage)
-        {
-            double topPlayerTotalDamage = this.combat.Players[sortCache.First()].TotalDamage == 0 ? 1 : this.combat.Players[sortCache.First()].TotalDamage;
-            targetInfo[player.Name].DPS = player.DPS;
-            targetInfo[player.Name].PctBar = (player.TotalDamage / topPlayerTotalDamage) * 100;
-            targetInfo[player.Name].TotalDMG = player.TotalDamage;
-            targetInfo[player.Name].Position = sortCache.IndexOf(player.Id) + 1;
-            transitionTimer = 0.0f;
-
-            PluginManager.Instance.PluginLog.Info($"Lerping {player.Name} to {targetInfo[player.Name].PctBar}");
-        }
     }
 
     public void Draw()
@@ -141,13 +79,82 @@ public class LazerOverlay : IMeterwayOverlay
         {
             Player player = combat.Players[id];
             DoLerpPlayerData(player);
-            DrawPlayerLine(lerpedInfo[player.Name], player);
+            DrawPlayerLine(lerpedInfo[player.Id], player);
         }
     }
 
-    public void Dispose()
+    public void Dispose() { }
+
+
+    private void UpdateWindowSize()
     {
+        Vector2 vMin = ImGui.GetWindowContentRegionMin();
+        Vector2 vMax = ImGui.GetWindowContentRegionMax();
+
+        vMin.X += ImGui.GetWindowPos().X;
+        vMin.Y += ImGui.GetWindowPos().Y;
+        vMax.X += ImGui.GetWindowPos().X;
+        vMax.Y += ImGui.GetWindowPos().Y;
+        this.WindowMin = vMin;
+        this.WindowMax = vMax;
     }
+
+
+    private void GenerateCombatLerping(Player player)
+    {
+        double topPlayerDamagePercentage = this.combat.Players[sortCache.First()].DamagePercentage;
+        if (topPlayerDamagePercentage == 0)
+        {
+            topPlayerDamagePercentage = 1; // Failsafe to avoid division by zero
+        }
+
+        lerpedInfo[player.Id] = new LerpPlayerData
+        {
+            DPS = player.DPS,
+            PctBar = player.DamagePercentage / topPlayerDamagePercentage * 100,
+            TotalDMG = player.TotalDamage,
+            Position = sortCache.IndexOf(player.Id) + 1
+        };
+
+        targetInfo[player.Id] = new LerpPlayerData
+        {
+            DPS = player.DPS,
+            PctBar = player.DamagePercentage / topPlayerDamagePercentage * 100,
+            TotalDMG = player.TotalDamage,
+            Position = sortCache.IndexOf(player.Id) + 1
+        };
+    }
+
+    private void DoLerpPlayerData(Player player)
+    {
+        if (!lerpedInfo.ContainsKey(player.Id))
+        {
+            GenerateCombatLerping(player);
+        }
+
+        if (transitionTimer < transitionDuration)
+        {
+            transitionTimer += ImGui.GetIO().DeltaTime;
+            double t = transitionTimer / transitionDuration;
+            lerpedInfo[player.Id].DPS = Helpers.Lerp(lerpedInfo[player.Id].DPS, targetInfo[player.Id].DPS, t);
+            lerpedInfo[player.Id].PctBar = Helpers.Lerp(lerpedInfo[player.Id].PctBar, targetInfo[player.Id].PctBar, t);
+            lerpedInfo[player.Id].TotalDMG = Helpers.Lerp(lerpedInfo[player.Id].TotalDMG, targetInfo[player.Id].TotalDMG, t);
+            lerpedInfo[player.Id].Position = Helpers.Lerp(lerpedInfo[player.Id].Position, targetInfo[player.Id].Position, t);
+        }
+
+        if (targetInfo[player.Id].TotalDMG <= player.TotalDamage)
+        {
+            double topPlayerTotalDamage = this.combat.Players[sortCache.First()].TotalDamage == 0 ? 1 : this.combat.Players[sortCache.First()].TotalDamage;
+            targetInfo[player.Id].DPS = player.DPS;
+            targetInfo[player.Id].PctBar = (player.TotalDamage / topPlayerTotalDamage) * 100;
+            targetInfo[player.Id].TotalDMG = player.TotalDamage;
+            targetInfo[player.Id].Position = sortCache.IndexOf(player.Id) + 1;
+            transitionTimer = 0.0f;
+
+            //PluginManager.Instance.PluginLog.Info($"Lerping {player.Name} to {targetInfo[player.Name].PctBar}");
+        }
+    }
+
     private void DrawPlayerLine(LerpPlayerData data, Player player)
     {
         float lineHeight = ImGui.GetFontSize() + 5;
