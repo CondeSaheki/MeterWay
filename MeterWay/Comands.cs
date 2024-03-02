63e4211b22e819fc6f08460039984751fe3a0b9d
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 <48620486+CondeSaheki@users.noreply.github.com>
2024-03-02 07:18:31 -0300

    Simplified commands

--- MeterWay/Comands.cs
+++ MeterWay/Comands.cs
@@ -2,9 +2,9 @@ using System.Collections.Generic;
 using System;
 using MeterWay.Managers;
 using Dalamud.Game.Command;
-using MeterWay.Windows;
 using Dalamud.Game.Text.SeStringHandling;
 using Dalamud.Game.Text.SeStringHandling.Payloads;
+using System.Text;
 
 namespace MeterWay;
 
@@ -16,93 +16,72 @@ public class Commands : IDisposable
     public Commands(Plugin plugin)
     {
         Plugin = plugin;
-        Dictionary<string, Action> args = [];
-
-        // add command
-        InterfaceManager.Inst.CommandManager.AddHandler(CommandName, new CommandInfo((string command, string arg) =>
-        {
-            if (args.TryGetValue(arg, out Action? value)) value.Invoke();
-            else
-            {
-                var text = $"The argument \'{arg}\' does not exist! consult \'{CommandName} help\'.";
-                InterfaceManager.Inst.ChatGui.Print(new SeString(new UIForegroundPayload(539), new TextPayload(text), new UIForegroundPayload(0)));
-            }
-        })
+        InterfaceManager.Inst.CommandManager.AddHandler(CommandName, new CommandInfo((string command, string arg) => { OnCommand(arg); })
         {
             HelpMessage = $"Display {Plugin.Name} main window.\nAditional help with the command \'{CommandName} help\'."
         });
+    }
 
-        // add command arg
-        string AddArg(string arg, string help, Action fn)
+    private static string HelpMessage() 
+    {
+        StringBuilder Message = new();
+        void helpMessageBuilder(string arg, string help) => Message.Append($"{CommandName}{(arg != "" ? " " : "")}{arg} -> {help}\n");
+
+        helpMessageBuilder("", "Display MeterWay main window.");
+        helpMessageBuilder("config", "Display MeterWay configuration window.");
+        helpMessageBuilder("connect", "Try to connect to IINACT.");
+        helpMessageBuilder("disconnect", "Diconnect from IINACT.");
+        helpMessageBuilder("reconnect", "Try to reconnect to IINACT.");
+        helpMessageBuilder("status", "Display the connection to IINACT status.");
+        helpMessageBuilder("help", "Display this help message");
+
+        Message.Remove(Message.Length - 1, 1);
+        
+        return Message.ToString();
+    }
+
+    private void OnCommand(string arg)
+    {
+        List<string> args = [.. arg.Split(' ')];
+        Action handler = args[0] switch
         {
-            args.Add(arg, fn);
-            return $"{CommandName}{(arg != "" ? " " : "")}{arg} -> {help}\n";
-        }
+            "" when args.Count == 1 => () => { Plugin.MainWindow.IsOpen = true; },
+            "config" when args.Count == 1 => () => { Plugin.ConfigWindow.IsOpen = true; },
+            "connect" when args.Count == 1 => Plugin.IpcClient.Connect,
+            "disconnect" when args.Count == 1 => Plugin.IpcClient.Disconnect,
+            "reconnect" when args.Count == 1 => Plugin.IpcClient.Reconnect,
+            "status" when args.Count == 1 => () =>
+            {
+                var msg = $"MeterWay is {(Plugin.IpcClient.Status() ? "connected" : "disconnected")} to IINACT.";
+                InterfaceManager.Inst.ChatGui.Print(msg);
+            },
+            "help" when args.Count == 1 => () => { InterfaceManager.Inst.ChatGui.Print(HelpMessage()); },
 
-        string help = AddArg("", "Display MeterWay main window.", () =>
-        {
-            Plugin.MainWindow.IsOpen = true;
-        });
+            #if DEBUG
 
-        help += AddArg("config", "Display MeterWay configuration window.", () =>
-        {
-            Plugin.ConfigWindow.IsOpen = true;
-        });
+            "_debug" when args.Count == 1 => () => { Plugin.DebugWindow.IsOpen = true; },
+            "_start" when args.Count == 1 => () => { EncounterManager.Start(); },
+            "_Stop" when args.Count == 1 => () => { EncounterManager.Stop(); },
+            "_clear" when args.Count == 1 => () =>
+            {
+                EncounterManager.Inst.encounters.Clear();
+                EncounterManager.Inst.encounters.Add(new Data.Encounter());
+            },
+            
+            #endif
 
-        help += AddArg("status", "Display the connection to IINACT status.", () =>
-        {
-            var msg = $"MeterWay is {(Plugin.IpcClient.Status() ? "connected" : "disconnected")} to IINACT.";
-            InterfaceManager.Inst.ChatGui.Print(msg);
-        });
-
-        help += AddArg("connect", "Try to connect to IINACT.", () =>
-        {
-            Plugin.IpcClient.Connect();
-        });
-
-        help += AddArg("disconnect", "Diconnect from IINACT.", () =>
-        {
-            Plugin.IpcClient.Disconnect();
-        });
-
-        help += AddArg("reconnect", "Try to reconnect to IINACT.", () =>
-        {
-            Plugin.IpcClient.Reconnect();
-        });
-
-        help += AddArg("help", "Display this help message", () =>
-        {
-            InterfaceManager.Inst.ChatGui.Print(help.Remove(help.Length - 1));
-        });
-
-#if DEBUG
-
-        args.Add("_debug", () =>
-        {
-            Plugin.DebugWindow.IsOpen = true;
-        });
-
-        args.Add("_clear", () =>
-        {
-            EncounterManager.Inst.encounters.Clear();
-            EncounterManager.Inst.encounters.Add(new Data.Encounter());
-        });
-
-        args.Add("_Stop", () =>
-        {
-            EncounterManager.Stop();
-        });
-
-        args.Add("_start", () =>
-        {
-            EncounterManager.Start();
-        });
-
-#endif
+            _ => () =>
+            {
+                var text = $"Invalid command \'{args}\' consult \'{CommandName} help\'.";
+                InterfaceManager.Inst.ChatGui.Print(new SeString(new UIForegroundPayload(539), new TextPayload(text), new UIForegroundPayload(0)));
+            }
+        };
+        handler();
     }
 
     public void Dispose()
     {
+        GC.SuppressFinalize(this);
         InterfaceManager.Inst.CommandManager.RemoveHandler(CommandName);
     }
 }
