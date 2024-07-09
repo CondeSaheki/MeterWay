460edc24bd3711838b971ecc61d5289dfea8fabb
maotovisk <me@maot.dev>
2024-07-09 01:59:37 -0300

    fix(Pet): add default fields

--- MeterWay/Managers/EncounterManager/EncounterData/Pet.cs
+++ MeterWay/Managers/EncounterManager/EncounterData/Pet.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using Dalamud.Game.ClientState.Objects.Types;
@@ -9,21 +10,21 @@ namespace MeterWay.Data;
 
 public class Pet : IPet
 {
-    public IPlayer? Owner {get;set;}
+    public IPlayer? Owner { get; set; } = null;
 
     public uint Id {get;set;}
 
-    public string Name {get;set;}
+    public string Name {get;set;} = string.Empty;
 
-    public Damage DamageDealt {get;set;}
+    public Damage DamageDealt { get; set; } = new();
 
-    public Damage DamageReceived {get;set;}
+    public Damage DamageReceived { get; set; } = new();
 
-    public Heal HealDealt {get;set;}
+    public Heal HealDealt { get; set; } = new();
 
-    public Heal HealReceived {get;set;}
+    public Heal HealReceived { get; set; } = new();
 
-    public PerSeconds PerSeconds {get;set;}
+    public PerSeconds PerSeconds { get; set; } = new();
 
     public void Calculate()
     {
